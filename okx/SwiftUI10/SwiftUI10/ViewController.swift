54a3633f7f64e5c0e0c3a145a880723ee168a0aa
jiangtao <202121101@any3.com>
2026-01-22 14:27:36 +0800

    develop: demo提交

---
+++ okx/SwiftUI10/SwiftUI10/ViewController.swift
@@ -0,0 +1,155 @@
+//
+//  ViewController.swift
+//  SwiftUI10
+//
+//  Created by CQCA202121101_2 on 2025/11/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView = UITableView()
+    private let demoList = ["Demo 1: 基础UI展示", "Demo 2: 动画效果", "Demo 3: 网络请求示例", "Demo 4: 数据存储示例"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        title = "Demo 入口"
+        view.backgroundColor = .white
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return demoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = demoList[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        var targetVC: UIViewController
+        
+        switch indexPath.row {
+        case 0:
+            targetVC = Demo1ViewController()
+        case 1:
+            targetVC = Demo2ViewController()
+        case 2:
+            targetVC = Demo3ViewController()
+        case 3:
+            targetVC = Demo4ViewController()
+        default:
+            return
+        }
+        
+        navigationController?.pushViewController(targetVC, animated: true)
+    }
+}
+
+// Demo 1 ViewController
+class Demo1ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Demo 1: 基础UI展示"
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.text = "这是基础UI展示示例"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18)
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
+// Demo 2 ViewController
+class Demo2ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Demo 2: 动画效果"
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.text = "这是动画效果示例"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18)
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
+// Demo 3 ViewController
+class Demo3ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Demo 3: 网络请求示例"
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.text = "这是网络请求示例"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18)
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
+// Demo 4 ViewController
+class Demo4ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Demo 4: 数据存储示例"
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.text = "这是数据存储示例"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18)
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
