4ae066d27a764d9b505625359d2fe639907691a9
jiangtao <202121101@any3.com>
2026-01-27 17:15:08 +0800

    prepare: MVC MVP MVVM 编译错误解决

--- okx/SwiftDemo/SwiftDemo/Main/POPViewController.swift
+++ okx/SwiftDemo/SwiftDemo/Main/POPViewController.swift
@@ -5,8 +5,7 @@
 //  Created by CQCA202121101_2 on 2026/1/23.
 //
 
-//cd /Users/taojiang/Desktop/poxiao/architecture/okx/SwiftDemo && xcodebuild -workspace SwiftDemo.xcworkspace -scheme SwiftDemo -configuration Debug -sdk iphoneos build
-//xcodebuild -workspace SwiftDemo.xcworkspace -scheme SwiftDemo -configuration Debug -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 16 Pro Max,OS=18.6' build
+
 
 import UIKit
 
