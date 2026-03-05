dd589f3723d89df009dd205f6e3e8c7069540aef
jiangtao <202121101@any3.com>
2026-03-05 18:02:14 +0800

    prepare:  补充RXSwift example 1

--- okx/SwiftDemo/SwiftDemo/SwiftFoundation/RXSwiftExamples/RXSwiftExampleViewController.swift
+++ okx/SwiftDemo/SwiftDemo/SwiftFoundation/RXSwiftExamples/RXSwiftExampleViewController.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class RXSwiftExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        
+        let example = Example1_RXSwift_Basic_Obervale()
+        example.test()
+    }
+    
+}
+
+
+class Example1_RXSwift_Basic_Obervale {
+    let disposeBag = DisposeBag()
+    
+    func test() {
+        let observable = Observable<Int>.just(1)
+        observable.subscribe { value in
+            print("just1: \(value)")
+        } onCompleted: {
+            print("完成1")
+        }.disposed(by: disposeBag)
+        
+        Observable.of(1, 2, 3)
+            .subscribe {
+                print("of2: \($0)")
+            }
+            .disposed(by: disposeBag)
+        
+        Observable<Void>.empty()
+            .subscribe {
+                print("完成3")
+            }.disposed(by: disposeBag)
+    }
+    
+    
+    
+}
