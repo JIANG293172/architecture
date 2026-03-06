da7f1170041eba88a7db053362a5c2299e3dbb29
jiangtao <202121101@any3.com>
2026-03-06 14:49:49 +0800

    prepare:  补充RXSwift example

--- okx/SwiftDemo/SwiftDemo/SwiftFoundation/RXSwiftExamples/RXSwiftExampleViewController.swift
+++ okx/SwiftDemo/SwiftDemo/SwiftFoundation/RXSwiftExamples/RXSwiftExampleViewController.swift
@@ -18,12 +18,10 @@ class RXSwiftExampleViewController: UIViewController {
         
         self.view.backgroundColor = .white
         
-        let example = Example19_RXSwift_Operator_Retry()
+        let example = Example40_RXSwift_Higher_Order_Skip()
         example.test()
     }
-    
 }
-
 
 class Example1_RXSwift_Basic_Obervale {
     func test() {
@@ -463,28 +461,316 @@ class Example27_RXSwift_UI_Slider {
 }
 
 class Example28_RXSwift_UI_Alert: UIViewController {
-    
-    func test() {
-        
-        
+    override func viewDidLoad() {
+        super.viewDidLoad()
         let btn = UIButton()
+        btn.frame = CGRect.init(x: 0, y: 0, width: 100, height: 100)
+        self.view.addSubview(btn)
+        btn.setTitle("点击", for: .normal)
+        btn.setTitleColor(.blue, for: .normal)
+        btn.backgroundColor = .white
+        
         btn.rx.tap
             .flatMapLatest { [weak self] _ -> Observable<UIAlertAction> in
-                let alert = UIAlertController(title: "提示", message: "", preferredStyle: .alert)
-                alert.addAction(.init(title: "确定", style: .default))
-                self?.present(alert, animated: true)
-                return alert.rx.action
+                guard let self else { return .empty() }
+
+                return Observable<UIAlertAction>.create { observer in
+                    let alert = UIAlertController(title: "提示", message: "", preferredStyle: .alert)
+                    let ok = UIAlertAction(title: "确定", style: .default) { action in
+                        observer.onNext(action)
+                        observer.onCompleted()
+                    }
+                    alert.addAction(ok)
+                    self.present(alert, animated: true)
+
+                    return Disposables.create {
+                        alert.dismiss(animated: true)
+                    }
+                }
             }
-            .subscribe {
-                print("\($0)")
-            }
+            .subscribe(onNext: { action in
+                print("\(action)")
+            })
             .disposed(by: disposeBag)
     }
     
+    func test() {
+
+        
+    }
     
 }
 
 
 
+//import Combine
+//
+//var cancellables = Set<AnyCancellable>()
+//
+///// 使用 Publishers.Create 实现（底层自定义，对应 RxSwift 的 Observable.create）
+//func request(with url: String) -> AnyPublisher<Data, Never> {
+//    // Create 闭包接收：发送器(Sink) + 取消器(Cancellable)
+//    Publishers.Create<Data, Never> { subscriber in
+//        // 发送值（对应 RxSwift 的 obs.onNext）
+//        subscriber.receive(Data())
+//        // 发送完成（对应 RxSwift 的 obs.onCompleted）
+//        subscriber.receive(completion: .finished)
+//        
+//        // 返回取消逻辑（对应 RxSwift 的 Disposables.create）
+//        return AnyCancellable {
+//            print("请求被取消（Create）")
+//        }
+//    }
+//    .eraseToAnyPublisher()
+//}
+
+class Example29_RXSwift_Network_Request {
+    func test() {
+  
+    }
+    
+    func request(url: String) -> Observable<Data> {
+        
+        Observable.create { obs in
+            
+            obs.onNext(Data())
+            obs.onCompleted()
+            
+            
+            return Disposables.create()
+            
+        }
+    }
+}
 
 
+
+class Example31_RXSwift_Higher_Order_Share {
+    
+    func test() {
+        let ob = Observable.just(100).share()
+        
+        ob.subscribe {
+            print("订阅 1， \($0)")
+        }
+        .disposed(by: disposeBag)
+        
+        ob.subscribe {
+            print("订阅2 \($0)")
+        }
+        .disposed(by: disposeBag)
+    }
+}
+
+
+class Example32_RXSwift_Order_Multicase {
+    
+    func test() {
+        let sub = PublishSubject<Int>()
+        let ob = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+            .multicast(sub)
+        ob.subscribe {
+            print("调用 \($0)")
+        }.disposed(by: disposeBag)
+        
+        ob.connect().disposed(by: disposeBag)
+    }
+}
+
+class Example33_RXSwift_Higher_Order_Deferred {
+    
+    func test() {
+        Observable<Int>.deferred {
+            .just(Int.random(in: 0...10))
+        }
+        .subscribe {
+            print("随机 \($0)")
+        }
+        .disposed(by: disposeBag)
+    }
+}
+
+class Example34_RXSwift_Higher_Order_Amb {
+    
+    func test() {
+        let a = Observable.just(1).delay(.seconds(2), scheduler: MainScheduler.instance)
+        let b = Observable.just(2).delay(.seconds(1), scheduler: MainScheduler.instance)
+        
+        a.amb(b).subscribe {
+            print("最快 \($0)")
+        }.disposed(by: disposeBag)
+    }
+}
+
+class Example35_RXSwift_Higher_Order_Timeout {
+    func test() {
+        
+        Observable<Int>.never()
+            .timeout(.seconds(2), scheduler: MainScheduler.instance)
+            .subscribe {
+                print("超时 \($0)")
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
+class Example36_RXSwift_Higher_Order_Delay {
+    func test() {
+        
+        Observable.just(1)
+            .delay(.seconds(1), scheduler: MainScheduler.instance)
+            .subscribe {
+                print("延迟1")
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
+class Example_RXSwift_Higher_Order_Buffer {
+    
+    func test() {
+        Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+            .buffer(timeSpan: .seconds(3), count: 2, scheduler: MainScheduler.instance)
+            .subscribe {
+                print("批量 \($0)")
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
+class Example39_RXSwift_Higher_Order_Window {
+    
+    func test() {
+        Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+            .window(timeSpan: .seconds(3), count: 2, scheduler: MainScheduler.instance)
+            .subscribe ( onNext: { w in
+                w.asObservable()
+                    .subscribe {
+                        print("\($0)")
+                    }
+                    .disposed(by: disposeBag)
+            })
+            .disposed(by: disposeBag)
+        
+    }
+}
+
+class Example40_RXSwift_Higher_Order_Skip {
+    
+    func test() {
+        
+        Observable.of(1, 2, 3, 4)
+            .skip(2)
+            .subscribe {
+                print("跳过2 \($0)")
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
+class Example41_RXSwift_UserListViewModel {
+    
+    let refersh = PublishSubject<Void>()
+    let users = BehaviorSubject<[String]>(value: [])
+    let isLoding = BehaviorSubject<Bool>(value: false)
+    
+    init() {
+        refersh.flatMapLatest { _ -> Observable<[String]>  in
+            return self.fetchUsers()
+        }
+        .bind(to: users)
+        .disposed(by: disposeBag)
+        
+        refersh.flatMapLatest { _ -> BehaviorSubject<[String]> in
+            return self.fetchUsers3()
+        }.bind(to: users)
+            .disposed(by: disposeBag)
+        
+    }
+    
+    func fetchUsers1() -> Observable<[String]> {
+        return Observable.just(["用户1", "用户2"]).delay(.seconds(1), scheduler: MainScheduler.instance)
+    }
+    
+    func fetchUsers() -> Observable<[String]> {
+        
+        return Observable<[String]>.create { observer in
+            let workItem = DispatchWorkItem {
+                let usersa = ["用户3", "用户4"]
+                observer.onNext(usersa)
+                observer.onCompleted()
+            }
+            DispatchQueue.global().asyncAfter(deadline: .now() + 1, execute: workItem)
+            
+            return Disposables.create {
+                workItem.cancel()
+                print("页码")
+            }
+        }
+    }
+    
+    func fetchUsers3() -> BehaviorSubject<[String]> {
+        
+        let subject = BehaviorSubject<[String]>(value: [""])
+        let workItem = DispatchWorkItem {
+            
+            let users = ["用户5", "用户6"]
+            subject.onNext(users)
+            subject.onCompleted()
+        }
+        
+        DispatchQueue.global().asyncAfter(deadline: .now()+1, execute: workItem)
+        
+        
+//        subject.disposable = Disposables.create {
+//            workItem.cancel()
+//        }
+//        
+        return subject
+    }
+}
+
+extension Reactive where Base: UIAlertController {
+    
+    var action: Observable<UIAlertAction> {
+        Observable.create { obs in
+            Disposables.create()
+        }
+    }
+}
+
+class Example42_RXSwift_Architecture_Coordinator {
+    
+    
+    
+}
+
+
+class Example43_RXSwift_Eror_Handling_RetryWhen {
+    
+    func test() {
+        
+        Observable<Any>.error(NSError(domain: "", code: 0))
+            .retry { err in
+                err.flatMapLatest { _ in
+                    Observable<Int>.timer(.seconds(1), scheduler: MainScheduler.instance)
+                }
+            }
+            .subscribe()
+            .disposed(by: disposeBag)
+    }
+}
+
+//class Example44_RXSwift_Error_Handling_MapError {
+//    
+//    func test() {
+//        
+//        Observable<Any>.error(NSError(domain: "", code: 0))
+//            .mapError { _ in
+//                NSError(domain: "", code: 0)
+//            }
+//            .subscribe()
+//            .disposed(by: disposeBag)
+//    }
+//    
+//}
